447cf3d2a9a4f803fc115f2d000154d066e318de
Kuroollo <140017613+Kuroollo@users.noreply.github.com>
2024-06-10 14:45:21 -0300

    Images added

--- src/JS/script.js
+++ src/JS/script.js
@@ -4,6 +4,5 @@ const show = document.querySelector("#show")
 
 designshowbuton.addEventListener("click" , () =>{
     show.classList.toggle("show")
-
-
+    designshowbuton.style.display = "none"
 })
